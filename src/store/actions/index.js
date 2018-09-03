846c31c0474fd0614102f693fbd42a51227de8d9
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2018-09-03 18:39:44 +0100

    replace thunk with sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,16 +1,29 @@
 export { 
     addIngredient,
     removeIngredient,
-    initIngredients
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
 } from './burgerBuilder';
 export { 
     purchaseBurger,
+    purchaseBurderStart,
+    purchaseBurgerSuccess,
+    purchaseBurgerFail,
     purchaseInit,
-    fetchOrders
+    fetchOrders,
+    fetchOrdersStart,
+    fetchOrdersSuccess,
+    fetchOrdersFail
 } from './order';
 export {
     auth,
+    authStart,
+    authSuccess,
+    authFail,
     logout,
+    logoutSucceed,
     setAuthRedirectPath,
+    checkAuthTimeout,
     authCheckState
 } from './auth';
---
+++ src/store/sagas/order.js
@@ -0,0 +1,32 @@
+import { put } from 'redux-saga/effects'
+
+import axios from '../../axios-orders';
+import * as actions from '../actions/index';
+
+export function* purchaseBurgerSaga({ token, orderData }) {
+    yield put(actions.purchaseBurderStart());
+    try {
+        const response = yield axios.post('/orders.json?auth=' + token, orderData);
+        yield put(actions.purchaseBurgerSuccess(response.data.name, orderData));
+    } catch(error) {
+        yield put(actions.purchaseBurgerFail(error));
+    }
+}
+
+export function* fetchOrdersSaga({ token, userId }) {
+    yield put(actions.fetchOrdersStart());
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+    try {
+        const response = yield axios.get('/orders.json' + queryParams);
+        const fetchedOrders = [];
+        for (let key in response.data) {
+            fetchedOrders.push({ 
+                ...response.data[key],
+                id: key
+            });
+        }
+        yield put(actions.fetchOrdersSuccess(fetchedOrders));
+    } catch(error) {
+        yield put(actions.fetchOrdersFail(error));
+    }
+}
