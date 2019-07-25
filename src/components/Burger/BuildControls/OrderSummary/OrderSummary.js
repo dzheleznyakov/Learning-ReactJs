237e617330c731d1bb72e14cdde35c74e326601f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-07-25 06:57:37 +0100

    Modal to hooks (React.memo)

--- src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
+++ src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
@@ -3,25 +3,25 @@ import React from 'react';
 import Button from '../../../UI/Button/Button';
 
 const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingKey => (
-            <li key={`orderSummary${ingKey}`}><span 
-                style={{textTransform: 'capitalize'}}>{ingKey}:</span> {props.ingredients[ingKey]}
-            </li>
-        ));
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} BYR</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </React.Fragment>
-    )
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(ingKey => (
+      <li key={`orderSummary${ingKey}`}><span 
+        style={{textTransform: 'capitalize'}}>{ingKey}:</span> {props.ingredients[ingKey]}
+      </li>
+    ));
+  return (
+    <React.Fragment>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)} BYR</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+    </React.Fragment>
+  )
 };
 
 export default orderSummary;
